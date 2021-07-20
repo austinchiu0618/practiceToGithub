b13f06848c39785142b2b426e96289d210157f2e
AustinChiu <beyourgoodangel@gmail.com>
2021-07-21 00:52:41 +0800

    1st

---
+++ vue-tictactoe/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Board />
+</template>
+
+<script>
+import Board from "./components/Board.vue";
+
+export default {
+  name: "App",
+  components: {
+    Board,
+  },
+};
+</script>
+
+
